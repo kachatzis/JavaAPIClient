1406f2932da63d6fc2392ab402962918b6e654cb
Konstantinos Chatzis <kostaschatzis01@gmail.com>
2018-10-19 01:59:01 +0300

    Setup Configuration

---
+++ src/com/geniedev/APIClient/ClientConfig.java
@@ -0,0 +1,8 @@
+package com.geniedev.APIClient;
+
+public class ClientConfig {
+
+	public static String API_KEY = "<KEY>";
+	public static String BASE_URL = "http://<URL>/api/v2/<database>/_table/";
+	
+}
